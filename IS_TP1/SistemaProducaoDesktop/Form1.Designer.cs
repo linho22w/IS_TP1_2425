6433f91a5f241f994de99e3196af7dd0536619cd
antoniotrancoso01 <al2024116930@alunos.utad.pt>
2025-03-27 16:18:56 +0000

    aplicaçao de gerar finalizada

--- IS_TP1/SistemaProducaoDesktop/Form1.Designer.cs
+++ IS_TP1/SistemaProducaoDesktop/Form1.Designer.cs
@@ -31,15 +31,16 @@
             btnGerar = new Button();
             dgvProdutos = new DataGridView();
             btnExecutarSikuli = new Button();
+            button1 = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvProdutos).BeginInit();
             SuspendLayout();
             // 
             // btnGerar
             // 
-            btnGerar.Location = new Point(22, 21);
-            btnGerar.Margin = new Padding(4, 5, 4, 5);
+            btnGerar.Location = new Point(18, 17);
+            btnGerar.Margin = new Padding(3, 4, 3, 4);
             btnGerar.Name = "btnGerar";
-            btnGerar.Size = new Size(179, 50);
+            btnGerar.Size = new Size(143, 40);
             btnGerar.TabIndex = 0;
             btnGerar.Text = "Gerar Produto";
             btnGerar.UseVisualStyleBackColor = true;
@@ -50,34 +51,45 @@
             dgvProdutos.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dgvProdutos.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             dgvProdutos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvProdutos.Location = new Point(22, 100);
-            dgvProdutos.Margin = new Padding(4, 5, 4, 5);
+            dgvProdutos.Location = new Point(18, 80);
+            dgvProdutos.Margin = new Padding(3, 4, 3, 4);
             dgvProdutos.Name = "dgvProdutos";
             dgvProdutos.RowHeadersWidth = 62;
-            dgvProdutos.Size = new Size(794, 338);
+            dgvProdutos.Size = new Size(635, 270);
             dgvProdutos.TabIndex = 1;
             // 
             // btnExecutarSikuli
             // 
-            btnExecutarSikuli.Location = new Point(621, 21);
-            btnExecutarSikuli.Margin = new Padding(4, 5, 4, 5);
+            btnExecutarSikuli.Location = new Point(497, 17);
+            btnExecutarSikuli.Margin = new Padding(3, 4, 3, 4);
             btnExecutarSikuli.Name = "btnExecutarSikuli";
-            btnExecutarSikuli.Size = new Size(179, 50);
+            btnExecutarSikuli.Size = new Size(143, 40);
             btnExecutarSikuli.TabIndex = 2;
             btnExecutarSikuli.Text = "Automatizar";
             btnExecutarSikuli.UseVisualStyleBackColor = true;
             btnExecutarSikuli.Click += btnExecutarSikuli_Click;
             // 
+            // button1
+            // 
+            button1.Location = new Point(178, 17);
+            button1.Name = "button1";
+            button1.Size = new Size(141, 40);
+            button1.TabIndex = 3;
+            button1.Text = "Parar Produção";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnParar_Click;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(842, 462);
+            ClientSize = new Size(674, 370);
+            Controls.Add(button1);
             Controls.Add(dgvProdutos);
             Controls.Add(btnGerar);
             Controls.Add(btnExecutarSikuli);
-            Margin = new Padding(4, 5, 4, 5);
-            MinimumSize = new Size(860, 507);
+            Margin = new Padding(3, 4, 3, 4);
+            MinimumSize = new Size(692, 415);
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Sistema de Produção";
@@ -90,5 +102,6 @@
         private Button btnGerar;
         private DataGridView dgvProdutos;
         private Button btnExecutarSikuli;
+        private Button button1;
     }
 }
--- IS_TP1/SistemaProducaoDesktop/Form1.cs
+++ IS_TP1/SistemaProducaoDesktop/Form1.cs
@@ -10,19 +10,42 @@ namespace SistemaProducaoDesktop
     public partial class Form1 : Form
     {
         private List<Produto> produtos = new List<Produto>();
+        
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void btnGerar_Click(object sender, EventArgs e)
-        {
-            Produto novoProduto = Produto.GerarProdutoAleatorio();
-            produtos.Insert(0, novoProduto);
+        private Random random = new Random();
+        private bool gerando = false;
 
-            dgvProdutos.DataSource = null;
-            dgvProdutos.DataSource = produtos;
+        private async void btnGerar_Click(object sender, EventArgs e)
+        {
+            btnGerar.Enabled = false;
+            gerando = true;
+
+            while (gerando)
+            {
+                // Cria e adiciona o produto
+                Produto novo = Produto.GerarProdutoAleatorio();
+                produtos.Insert(0, novo);
+
+                // Atualiza o DataGridView
+                dgvProdutos.DataSource = null;
+                dgvProdutos.DataSource = produtos;
+
+                // Espera tempo aleatório
+                await Task.Delay(novo.Tempo_Producao*1000);
+            }
+
+            btnGerar.Enabled = true;
+        }
+
+        private void btnParar_Click(object sender, EventArgs e)
+        {
+            gerando = false;
+            btnGerar.Enabled = true;
         }
 
 
